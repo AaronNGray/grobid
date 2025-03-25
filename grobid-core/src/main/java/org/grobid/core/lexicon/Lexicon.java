ea718de23fb68bb86c16f897db5e82ae44643f93
Luca Foppiano <luca@foppiano.org>
2025-03-26 08:13:23 +0900

    overrule destination when the extracted URL is larger

--- grobid-core/src/main/java/org/grobid/core/lexicon/Lexicon.java
+++ grobid-core/src/main/java/org/grobid/core/lexicon/Lexicon.java
@@ -1416,6 +1416,10 @@ public class Lexicon {
             OffsetPosition position = new OffsetPosition();
             position.start = startPos;
             position.end = endPos;
+            if (destination != null
+                && StringUtils.length(destination) < LayoutTokensUtil.toText(urlTokens).substring(position.start, position.end).length()) {
+                destination = null;
+            }
             resultPositions.add(Pair.of(position, destination));
         }
         return resultPositions;
